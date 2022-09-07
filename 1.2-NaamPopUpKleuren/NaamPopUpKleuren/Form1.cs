6c237aea1e7d7bf8bb13c1d62ba12fb8561027fe
Alyxia <alyxia@riseup.net>
2022-09-07 22:01:39 +0200

    [Global] Pretty code

--- 1.2-NaamPopUpKleuren/NaamPopUpKleuren/Form1.cs
+++ 1.2-NaamPopUpKleuren/NaamPopUpKleuren/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NaamPopUpKleuren
@@ -22,44 +17,26 @@ namespace NaamPopUpKleuren
             MessageBox.Show($"De volgende text is ingevoerd in txtNaam: {txtNaam.Text}");
         }
 
-        private void btnShowStraatPopUp_Click(object sender, EventArgs e)
-        {
-            //MessageBox.Show($"De volgende text is ingevoerd in txtStraatNaam: {txtStraatNaam.Text}");
-        }
-
-        private void btnShowBeide_Click(object sender, EventArgs e)
-        {
-            //MessageBox.Show($"De volgende text is ingevoerd in txtNaam: {txtNaam.Text}");
-            //MessageBox.Show($"De volgende text is ingevoerd in txtStraatNaam: {txtStraatNaam.Text}");
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void benPaintButtons_Click(object sender, EventArgs e)
         {
             // We love type generics.
-            foreach(var button in this.Controls.OfType<Button>())
-            {
-                button.BackColor = Color.Red;
-            }
+            foreach (var button in Controls.OfType<Button>()) button.BackColor = Color.Red;
         }
 
         private void btnPaintBackground_Click(object sender, EventArgs e)
         {
-            this.BackColor = Color.Green;
+            BackColor = Color.Green;
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
             txtNaam.Text = "";
-            foreach (var button in this.Controls.OfType<Button>())
-            {
-                button.BackColor = SystemColors.ControlLight;
-            }
-            this.BackColor = SystemColors.Control;
+            foreach (var button in Controls.OfType<Button>()) button.BackColor = SystemColors.ControlLight;
+            BackColor = SystemColors.Control;
         }
     }
 }
--- 2.2-Tafels/Tafels/Form1.cs
+++ 2.2-Tafels/Tafels/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Tafels
@@ -39,11 +32,8 @@ namespace Tafels
 
             txtTafel.Text = "";
 
-            decimal num = decimal.Parse(txtGetal.Text);
-            for (int i = 1; i < 11; i++)
-            {
-                txtTafel.AppendText($"{i} * {num} = {i * num}" + System.Environment.NewLine);
-            }
+            var num = decimal.Parse(txtGetal.Text);
+            for (var i = 1; i < 11; i++) txtTafel.AppendText($"{i} * {num} = {i * num}" + Environment.NewLine);
         }
 
         private void btnReset_Click(object sender, EventArgs e)
