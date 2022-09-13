438353c69c6ba8801f62eb3630353840e40ddf0d
Alyxia Sother <alyxia@riseup.net>
2022-09-13 20:02:34 +0200

    [4.3] Add exercise

---
+++ 4.3-KleurenKiezen/KleurenKiezen/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace KleurenKiezen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbRed = new System.Windows.Forms.RadioButton();
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.rbYellow = new System.Windows.Forms.RadioButton();
+            this.rbGreen = new System.Windows.Forms.RadioButton();
+            this.rbOrange = new System.Windows.Forms.RadioButton();
+            this.pnlCheckedColor = new System.Windows.Forms.Panel();
+            this.btnSetColorPanel = new System.Windows.Forms.Button();
+            this.btnSetColorBackground = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            // rbRed
+            //
+            this.rbRed.AutoSize = true;
+            this.rbRed.Checked = true;
+            this.rbRed.Location = new System.Drawing.Point(12, 12);
+            this.rbRed.Name = "rbRed";
+            this.rbRed.Size = new System.Drawing.Size(62, 20);
+            this.rbRed.TabIndex = 0;
+            this.rbRed.TabStop = true;
+            this.rbRed.Text = "Rood";
+            this.rbRed.UseVisualStyleBackColor = true;
+            //
+            // rbBlue
+            //
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(12, 38);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(64, 20);
+            this.rbBlue.TabIndex = 1;
+            this.rbBlue.Text = "Blauw";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            //
+            // rbYellow
+            //
+            this.rbYellow.AutoSize = true;
+            this.rbYellow.Location = new System.Drawing.Point(12, 64);
+            this.rbYellow.Name = "rbYellow";
+            this.rbYellow.Size = new System.Drawing.Size(57, 20);
+            this.rbYellow.TabIndex = 2;
+            this.rbYellow.Text = "Geel";
+            this.rbYellow.UseVisualStyleBackColor = true;
+            //
+            // rbGreen
+            //
+            this.rbGreen.AutoSize = true;
+            this.rbGreen.Location = new System.Drawing.Point(12, 90);
+            this.rbGreen.Name = "rbGreen";
+            this.rbGreen.Size = new System.Drawing.Size(65, 20);
+            this.rbGreen.TabIndex = 3;
+            this.rbGreen.Text = "Groen";
+            this.rbGreen.UseVisualStyleBackColor = true;
+            //
+            // rbOrange
+            //
+            this.rbOrange.AutoSize = true;
+            this.rbOrange.Location = new System.Drawing.Point(12, 116);
+            this.rbOrange.Name = "rbOrange";
+            this.rbOrange.Size = new System.Drawing.Size(68, 20);
+            this.rbOrange.TabIndex = 4;
+            this.rbOrange.Text = "Oranje";
+            this.rbOrange.UseVisualStyleBackColor = true;
+            //
+            // pnlCheckedColor
+            //
+            this.pnlCheckedColor.Location = new System.Drawing.Point(154, 12);
+            this.pnlCheckedColor.Name = "pnlCheckedColor";
+            this.pnlCheckedColor.Size = new System.Drawing.Size(172, 135);
+            this.pnlCheckedColor.TabIndex = 5;
+            //
+            // btnSetColorPanel
+            //
+            this.btnSetColorPanel.Location = new System.Drawing.Point(12, 185);
+            this.btnSetColorPanel.Name = "btnSetColorPanel";
+            this.btnSetColorPanel.Size = new System.Drawing.Size(143, 23);
+            this.btnSetColorPanel.TabIndex = 0;
+            this.btnSetColorPanel.Text = "Kleur panel";
+            this.btnSetColorPanel.UseVisualStyleBackColor = true;
+            this.btnSetColorPanel.Click += new System.EventHandler(this.btnSetColorPanel_Click);
+            //
+            // btnSetColorBackground
+            //
+            this.btnSetColorBackground.Location = new System.Drawing.Point(12, 214);
+            this.btnSetColorBackground.Name = "btnSetColorBackground";
+            this.btnSetColorBackground.Size = new System.Drawing.Size(143, 23);
+            this.btnSetColorBackground.TabIndex = 1;
+            this.btnSetColorBackground.Text = "Kleur achtergrond";
+            this.btnSetColorBackground.UseVisualStyleBackColor = true;
+            this.btnSetColorBackground.Click += new System.EventHandler(this.btnSetColorBackground_Click);
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 249);
+            this.Controls.Add(this.btnSetColorPanel);
+            this.Controls.Add(this.btnSetColorBackground);
+            this.Controls.Add(this.pnlCheckedColor);
+            this.Controls.Add(this.rbOrange);
+            this.Controls.Add(this.rbGreen);
+            this.Controls.Add(this.rbYellow);
+            this.Controls.Add(this.rbBlue);
+            this.Controls.Add(this.rbRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbRed;
+        private System.Windows.Forms.RadioButton rbBlue;
+        private System.Windows.Forms.RadioButton rbYellow;
+        private System.Windows.Forms.RadioButton rbGreen;
+        private System.Windows.Forms.RadioButton rbOrange;
+        private System.Windows.Forms.Panel pnlCheckedColor;
+        private System.Windows.Forms.Button btnSetColorPanel;
+        private System.Windows.Forms.Button btnSetColorBackground;
+    }
+}
---
+++ 4.3-KleurenKiezen/KleurenKiezen/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KleurenKiezen
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        ///     Loop over all radio buttons and return the one that is checked.
+        /// </summary>
+        /// <returns><see cref="RadioButton"/></returns>
+        private RadioButton getSelected()
+        {
+            RadioButton res = null;
+            foreach (RadioButton rb in Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                {
+                    res = rb;
+                }
+            }
+            return res;
+
+        }
+
+        /// <summary>
+        ///     Get a <see cref="Color">Color</see> of the input string.
+        /// </summary>
+        /// <param name="input">String to get Color of</param>
+        /// <returns><see cref="Color"/></returns>
+        private Color getColor(string input)
+        {
+            Color col = Color.White;
+            switch (input)
+            {
+                case "Rood":
+                    col = Color.Red;
+                    break;
+                case "Blauw":
+                    col = Color.Blue;
+                    break;
+                case "Geel":
+                    col = Color.Yellow;
+                    break;
+                case "Groen":
+                    col = Color.Green;
+                    break;
+                case "Oranje":
+                    col = Color.Orange;
+                    break;
+            }
+            return col;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSetColorPanel_Click(object sender, EventArgs e)
+        {
+            RadioButton rb = getSelected();
+            Color col = getColor(rb.Text);
+            pnlCheckedColor.BackColor = col;
+        }
+
+        private void btnSetColorBackground_Click(object sender, EventArgs e)
+        {
+            RadioButton rb = getSelected();
+            Color col = getColor(rb.Text);
+            BackColor = col;
+        }
+    }
+}
